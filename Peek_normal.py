6bb4b210f74343dff4c3d9c0c7334e363e2cb45c
qianmoke <zzilovey@gmail.com>
2016-10-20 17:38:00 +0800

    change the impl of peek

--- Peek_normal.py
+++ Peek_normal.py
@@ -6,55 +6,35 @@ Created on Dec 21, 2015
 import os
 from config import dateEnd, dateStart, month
 from config import moniPath,peekPath
+from collections import OrderedDict
+from re import search
 
 def handle(text,fileName):
-    #global timetag,BANCS000,CMS00000,DFHTCL01,DFHTCL02,DFHTCL03,DFHTCL04,DFHTCL05,DFHTCL06,DFHTCL07,DFHTCL08,DFHTCL09,DFHTCL10,HDQS0000,RCPS0001,RCPS0002
-    #print line
-    #print openfile
+    peekDict = OrderedDict()
+    timetag = ""
     for line in text.splitlines():
-        montime = line.split(" Current time: ")
-        if(montime[0] == "=="):
-            timetag = montime[-1].strip()
-        linetmp=",".join(line.split())
-        if(linetmp.split(",")[0] == "TClassINFO01"):
-            linetmp2=linetmp.split(",")
-            print linetmp2
-            TClassINFO01=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO02"):
-            linetmp2=linetmp.split(",")
-            TClassINFO02=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO03"):
-            linetmp2=linetmp.split(",")
-            TClassINFO03=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO04"):
-            linetmp2=linetmp.split(",")
-            TClassINFO04=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO05"):
-            linetmp2=linetmp.split(",")
-            TClassINFO05=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO06"):
-            linetmp2=linetmp.split(",")
-            TClassINFO06=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO07"):
-            linetmp2=linetmp.split(",")
-            TClassINFO07=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO08"):
-            linetmp2=linetmp.split(",")
-            TClassINFO08=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO09"):
-            linetmp2=linetmp.split(",")
-            TClassINFO09=linetmp2[4]
-        elif(linetmp.split(",")[0] == "TClassINFO10"):
-            linetmp2=linetmp.split(",")
-            TClassINFO10=linetmp2[4]
-            timeTest = timetag.split("/")[2].split()[0]
-            timeMonth = timetag.split("/")[1]    
-            if ( dateStart <= int(timeTest) <= dateEnd and timeMonth == month):
-                print  timetag + ","  + str(TClassINFO01) + "," + str(TClassINFO02) + "," + str(TClassINFO03) + "," + str(TClassINFO04) + "," + str(TClassINFO05) + "," + str(TClassINFO06) + "," + str(TClassINFO07) + "," + str(TClassINFO08) + "," + str(TClassINFO09) + "," + str(TClassINFO10) 
-                rptFile = open(fileName + ".peek.csv","a")
-                rptFile.write(timetag + "," + str(TClassINFO01) + "," + str(TClassINFO02) + "," + str(TClassINFO03) + "," + str(TClassINFO04) + "," + str(TClassINFO05) + "," + str(TClassINFO06) + "," + str(TClassINFO07) + "," + str(TClassINFO08) + "," + str(TClassINFO09) + "," + str(TClassINFO10))
-                rptFile.write("\n")
-                rptFile.close() 
+        if (search("TClass", line) and ( not search("TClassINFO00",line))):
+                peekDict[line.split()[0]] = list()
+                peekDict[line.split()[0]].append(line.split()[4])            
+        else:
+            montime = line.split(" Current time: ")
+            if(montime[0] == "=="):
+                timetag = montime[-1].strip()
+    timeTest = timetag.split("/")[2].split()[0]
+    timeMonth = timetag.split("/")[1]    
+    if ( dateStart <= int(timeTest) <= dateEnd and timeMonth == month):
+        data = timetag
+        for key in peekDict.keys():
+            data += "," + str(peekDict[key][0])
+        print data
+        try:
+            rptFile = open(fileName + ".peek.csv","a")
+            rptFile.write(data)
+            rptFile.write("\n")
+        except Exception:
+            print Exception.message
+        finally:    
+            rptFile.close() 
                  
 def readFile(moniPath, peekPath, moniFile,outputFile): 
     #try:
@@ -62,7 +42,8 @@ def readFile(moniPath, peekPath, moniFile,outputFile):
         text = rptFile.read()
         words = text.split("====================================")
         for element in words:
-            handle(element,peekPath + "/" + outputFile)
+            if element != "\n":
+                handle(element,peekPath + "/" + outputFile)
         rptFile.close()
     #except:
     #    rptFile.close()     
