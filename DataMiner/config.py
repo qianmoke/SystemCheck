512b92e9274da8d5b4c154df3f86eb008126f810
qianmoke <zzilovey@gmail.com>
2016-11-04 10:51:43 +0800

    add multiprocess impl

--- DataMiner/config.py
+++ DataMiner/config.py
@@ -1,9 +1,9 @@
 pathDict={
-'moniPath':"G:\\Systemcheck\\MONI2",
+'moniPath':"G:\\Systemcheck\\MONI5",
 'reportPath':"G:\\Systemcheck\\report",
 'imagePath':"G:\\Systemcheck\\Web\\statics\\images",
 'templatePath': "G:\\Systemcheck\\template",
 'maxPath':"G:\\Systemcheck\\max",
 'templatePath':"G:\\Systemcheck\\Web\\templates"
 }
-period = ["09", 20, 26]
+period = ["10", 20, 26]
--- main.py
+++ main.py
@@ -4,35 +4,54 @@ from DataMiner.moni_excel import processFile,excelProcess,maxCal
 from DataMiner.exceltoimage import exceltoimage,imagechoose
 import os,re
 from Web.server import  main
-import thread
+import multiprocessing
+from time import sleep
+
+def processThread(fileDict, outputFile):
+    peekDict = defaultdict(OrderedDict)
+    tpsDict = defaultdict(OrderedDict)
+    for moniFile in fileDict[outputFile]:
+        processFile(pathDict['moniPath'] + '/' +moniFile, peekDict[outputFile],tpsDict[outputFile])
+    excelProcess(pathDict['reportPath'], peekDict, tpsDict)
+    maxCal(pathDict['maxPath'], peekDict, tpsDict)
+    
+def schedule(processDict):
+    alive = 0
+    for name in processDict.keys():
+        if processDict[name][0].is_alive():
+            alive += 1
+            
+    for name in processDict.keys():
+        if alive < 4 and processDict[name][1] == 0:
+            processDict[name][0].start()
+            processDict[name][1]=1
+            alive += 1  
+      
+    if alive == 0:
+        for xlsfile in os.listdir(pathDict['reportPath']):
+            print xlsfile
+            exceltoimage(pathDict['reportPath'], xlsfile, pathDict['imagePath'])    
+            for root,dirs,files in os.walk(pathDict['imagePath']):
+                for directory in dirs:
+                    for fileName in os.listdir(pathDict['imagePath']+"\\"+directory):
+                        imagechoose(pathDict['imagePath']+"\\"+directory, fileName)
+        main()
 
 if __name__ == "__main__":
-    '''fileDict = defaultdict(list)  
-    for root, dirs, files in os.walk(pathDict['moniPath']):
-        for moniFile in files:
-            if re.search("_", moniFile):
-                outputFile = "_".join(moniFile.split("_")[0:2])
-            else:
-                outputFile = "_".join(moniFile.split(".")[1:3])
-            fileDict[outputFile].append(moniFile)
+    fileDict = defaultdict(list)
+
+    processDict = defaultdict(list)  
+    for moniFile in os.listdir(pathDict['moniPath']):
+        if re.search("_", moniFile):
+            outputFile = "_".join(moniFile.split("_")[0:2])
+        else:
+            outputFile = "_".join(moniFile.split(".")[1:3])
+        fileDict[outputFile].append(moniFile)
     for outputFile in fileDict.keys():
-        peekDict = defaultdict(OrderedDict)
-        tpsDict = defaultdict(OrderedDict)
-        for moniFile in fileDict[outputFile]:
-            print moniFile
-            processFile(pathDict['moniPath'] + '/' +moniFile, peekDict[outputFile],tpsDict[outputFile])
-        #thread.start_new_thread(excelProcess, (pathDict['reportPath'], peekDict, tpsDict,) )
-        excelProcess(pathDict['reportPath'], peekDict, tpsDict) 
-        maxCal(pathDict['maxPath'], peekDict, tpsDict)
-    
-    for root,dirs,files in os.walk(pathDict['reportPath']):
-        for xlsfile in files:
-            print xlsfile
-            exceltoimage(pathDict['reportPath'], xlsfile, pathDict['imagePath'])
-    
-    for root,dirs,files in os.walk(pathDict['imagePath']):
-        for directory in dirs:
-            for root,dirs,files in os.walk(pathDict['imagePath']+"\\"+directory):
-                for fileName in files:
-                    imagechoose(pathDict['imagePath']+"\\"+directory, fileName)'''
+        #excelProcess(pathDict['reportPath'], peekDict, tpsDict) 
+        process=multiprocessing.Process(target = processThread, args=(fileDict, outputFile,))
+        processDict[process.name] += [process, 0]
+    while True:
+        schedule(processDict)
+        sleep(1)
     main()
